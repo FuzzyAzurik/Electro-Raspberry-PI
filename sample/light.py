e27c26908760680db5574d19aa1a8ac871e07db7
Jacob Wortmann <jacob@wortmann.dk>
2017-04-21 22:06:39 +0200

    added sensor script

--- sample/light.py
+++ sample/light.py
@@ -5,10 +5,10 @@ import time
 GPIO.setmode(GPIO.BCM)
 led = 4
 GPIO.setup(led, GPIO.OUT)
+
 # loop through 50 times, on/off for 1 second
 for i in range(10):
     GPIO.output(led,True)
     time.sleep(1)
     GPIO.output(led,False)
-    time.sleep(1)
 GPIO.cleanup()
